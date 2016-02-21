fc4f18b57b8d5732aff3de325c57423ddb42049c
Andy Miller <armil@armil-macbookpro.roam.corp.google.com>
2016-02-21 12:08:47 -0800

    Is compile

--- prometheus/prometheus.h
+++ prometheus/prometheus.h
@@ -7,12 +7,6 @@ class PrometheusClient {
                    const uint16_t port,
                    const String& job,
                    const String& instance);
-  ~PrometheusClient() { 
-    Clear();
-    Stop();
-  }
-  // Init the client - this actually connects to the prometheus server.
-  bool Init();
   // AddGauge adds a gauge metric to be updated to the prometheus server.
   //     "extra_keys" are just copied as-is into the args.
   void AddGauge(const String& name,
@@ -27,14 +21,10 @@ class PrometheusClient {
   // Send will post any unsent messages to the prometheus server and clear
   //     the list of pending messages.
   bool Send();
-  // Stop will stop the wifi client.
-  void Stop() {
-    client_.stop();
-  }
  private:
   const String server_;
   const uint16_t port;
   const String url_;
   String messages_;
-  WifiClient client_;
-}
+  WiFiClient client_;
+};
