47101432ae6a4d4235f12c1ad10ca18402e5da67
z2amiller <z2amiller@gmail.com>
2016-02-21 20:47:10 -0800

    Fix adc -> i2c

--- i2cbase/i2cbase.cpp
+++ i2cbase/i2cbase.cpp
@@ -12,29 +12,29 @@ i2cBase::Init() {
 }
 
 void i2cBase::write8(const uint8_t addr, const uint8_t data) {
-  Wire.beginTransmission(adc_addr_);
+  Wire.beginTransmission(i2c_addr);
   Wire.write(addr);
   Wire.write(data);
   Wire.endTransmission();
 }
 
 uint8_t i2cBase::read8(const uint8_t addr) {
-  Wire.beginTransmission(adc_addr_);
+  Wire.beginTransmission(i2c_addr_);
   Wire.write(addr);
   Wire.endTransmission();
-  Wire.beginTransmission(adc_addr_);
-  Wire.requestFrom(adc_addr_, (uint8_t)1);
+  Wire.beginTransmission(i2c_addr_);
+  Wire.requestFrom(i2c_addr_, (uint8_t)1);
   const uint8_t ret = Wire.read();
   Wire.endTransmission();
   return ret;
 }
 
 uint16_t i2cBase::read16(const uint8_t addr) {
-  Wire.beginTransmission(adc_addr_);
+  Wire.beginTransmission(i2c_addr_);
   Wire.write(addr);
   Wire.endTransmission();
-  Wire.beginTransmission(adc_addr_);
-  Wire.requestFrom(adc_addr_, (uint8_t)2);
+  Wire.beginTransmission(i2c_addr_);
+  Wire.requestFrom(i2c_addr_, (uint8_t)2);
   uint16_t ret = Wire.read();
   ret <<= 8;
   ret |= Wire.read();
